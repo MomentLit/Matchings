9dd641762e2156b970898d729b7506046e8b4bda
주환 <jhlarry1109@gmail.com>
2026-05-28 20:21:20 +0900

    init

---
+++ src/main/java/com/example/matchings/MatchingsApplication.java
@@ -0,0 +1,13 @@
+package com.example.matchings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchingsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatchingsApplication.class, args);
+    }
+
+}
